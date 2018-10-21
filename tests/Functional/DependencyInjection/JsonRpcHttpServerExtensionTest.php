8fdca5c9ae868c21b8699cae5e04e570392ec5d2
Yoanm <yoanm@users.noreply.github.com>
2018-10-21 14:20:31 +0200

    Improve

--- tests/Functional/DependencyInjection/JsonRpcHttpServerExtensionTest.php
+++ tests/Functional/DependencyInjection/JsonRpcHttpServerExtensionTest.php
@@ -1,12 +1,7 @@
 <?php
 namespace Tests\Functional\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Exception\LogicException;
-use Symfony\Component\DependencyInjection\Reference;
 use Tests\Common\DependencyInjection\AbstractTestClass;
-use Yoanm\SymfonyJsonRpcHttpServer\DependencyInjection\JsonRpcHttpServerExtension;
-use Yoanm\SymfonyJsonRpcHttpServer\Endpoint\JsonRpcHttpEndpoint;
-use Yoanm\SymfonyJsonRpcHttpServer\Resolver\ServiceNameResolver;
 use Yoanm\SymfonyJsonRpcParamsValidator\DependencyInjection\JsonRpcParamsValidatorExtension;
 
 /**
